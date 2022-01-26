1743a6d07dd270cedd460fd5b7b819a121bcfc30
jub3907 <side1923@naver.com>
2022-01-26 14:07:45 +0900

    아이디 입력후 전적 페이지로 이동하는 SummonerSearch 컴포넌트 추가

---
+++ src/components/common/Search/SummonerSearch.tsx
@@ -0,0 +1,40 @@
+import styles from './SummonerSearch.module.scss';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import { useRouter } from 'next/router';
+import { ChangeEvent, useCallback, useState } from 'react';
+import Path from 'config/path';
+
+const SubHeaderSearchForm = () => {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const onClick = () => {
+    router.push({
+      pathname: Path.summonerTotal,
+      query: { name },
+    });
+  };
+
+  return (
+    <div className={styles.layout}>
+      <input
+        type="text"
+        onChange={onChange}
+        placeholder="소환사명을 입력해 전적을 확인해 보세요."
+        className={styles.input}
+      />
+      <Divider sx={{ height: 28 }} orientation="vertical" />
+      <IconButton onClick={onClick} sx={{ p: '12px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </div>
+  );
+};
+
+export default SubHeaderSearchForm;
