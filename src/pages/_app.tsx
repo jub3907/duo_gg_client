cbe689f8425b76e67e5105a7e1d37b23743630db
jub3907 <side1923@naver.com>
2022-01-26 15:07:27 +0900

    Material UI의 기본 폰트를 Spoqa San Hans로 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material';
+import MuiTheme from 'config/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={MuiTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
