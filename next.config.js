db796f02b00fdace212ceccdc0944a01b51e36e7
jub3907 <side1923@naver.com>
2022-01-26 10:27:47 +0900

    sass 사용을 위한 config 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const path = require('path');
+
+module.exports = (phase) => {
+  return {
+    sassOptions: {
+      includePaths: [path.join(__dirname, './src/styles')],
+      prependData: `
+      @import "${(path.resolve(__dirname), './src/styles/_variables.scss')}";
+      @import "${(path.resolve(__dirname), './src/styles/_typography.scss')}";
+      `,
+    },
+  };
+};
