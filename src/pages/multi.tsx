e8d2aed0ac6a0cd81420e4d435f4c516afc376ca
jub3907 <side1923@naver.com>
2022-01-29 15:43:44 +0900

    멀티 서치 페이지 구현

--- src/pages/multi.tsx
+++ src/pages/multi.tsx
@@ -3,12 +3,23 @@ import Layout from '@common/Layout/Layout';
 import PageTitleLayout from '@common/Layout/PageTitleLayout';
 import MultiSearchForm from '@pages/Multi/Form/MultiSearchForm';
 import SummonerCard from '@pages/Summoner/Card/SummonerCard';
+import styles from '@pages/Multi/MultiSearch.module.scss';
+import MultiSummonerCard from '@pages/Multi/Card/MultiSummonerCard';
+
+const names = ['Duo TOP', 'Duo MID', 'Duo JUG', 'Duo BOT', 'Duo SUP'];
 
 const MultiSearchPage = () => {
   return (
     <Layout subHeader={<SubHeader />} activeMenu="multi">
       <PageTitleLayout title="멀티서치">
         <MultiSearchForm />
+        <div className={styles.grid}>
+          {names.map((name) => {
+            return (
+              <MultiSummonerCard name={name} key={`summoner-info-${name}`} />
+            );
+          })}
+        </div>
       </PageTitleLayout>
     </Layout>
   );
