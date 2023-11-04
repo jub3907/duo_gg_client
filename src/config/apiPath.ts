37fd97977fba6fb7d15f02471d49d91cf8dda606
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-11-04 15:03:03 +0900

    add api path

---
+++ src/config/apiPath.ts
@@ -0,0 +1,18 @@
+const apiPath = {
+  comment: '/comment/[name]',
+  deleteComment: '/comment/[commentId]',
+
+  league: '/league/[name]',
+  ranking: '/ranking',
+
+  masteryByPuuid: '/mastery/by-puuid/[puuid]',
+  masteryBySummoner: '/mastery/by-summoner/[summonerId]',
+  masteryByName: '/mastery/by-name/[name]',
+
+  match: '/match/[name]',
+  matchDetail: '/match/detail',
+
+  duo: '/duo',
+
+  summoner: '/summoner/[name]',
+};
